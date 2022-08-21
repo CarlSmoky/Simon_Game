728645c16139e91961475d491af414a3decc4d07
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-21 18:41:15 -0400

    Add click eventListner to buttons

--- game.js
+++ game.js
@@ -1,6 +1,33 @@
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 
+let gamePattern = [];
+let userClickedPattern = [];
+
+let level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+//Arrow function doesn't work here
+$(".btn").click(function () {
+
+  const userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+});
+
+
+
+
+
 const playSound = (name) => {
   const audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
